efb2859c3c6bde1c2f22dc1727d35cf9b2fdc7d2
humatrix-mobile <63693376+humatrix-mobile@users.noreply.github.com>
2021-01-07 15:49:54 +0700

    fixbug ios 14.
    File thumbnails<CGImage> are null when user allows humatrix to access photos (user choose "Select Image ...").

--- src/ios/ELCImagePicker/ELCAssetCell.m
+++ src/ios/ELCImagePicker/ELCAssetCell.m
@@ -19,6 +19,64 @@
 @implementation ELCAssetCell
 
 //Using auto synthesizers
+	
+// Helper methods for thumbnailForAsset:maxPixelSize:
+static size_t getAssetBytesCallback(void *info, void *buffer, off_t position, size_t count) {
+    ALAssetRepresentation *rep = (__bridge id)info;
+    
+    NSError *error = nil;
+    size_t countRead = [rep getBytes:(uint8_t *)buffer fromOffset:position length:count error:&error];
+    
+    if (countRead == 0 && error) {
+        // We have no way of passing this info back to the caller, so we log it, at least.
+        NSLog(@"thumbnailForAsset:maxPixelSize: got an error reading an asset: %@", error);
+    }
+    
+    return countRead;
+}
+
+static void releaseAssetCallback(void *info) {
+    // The info here is an ALAssetRepresentation which we CFRetain in thumbnailForAsset:maxPixelSize:.
+    // This release balances that retain.
+    CFRelease(info);
+}
+
+- (UIImage *)thumbnailForAsset:(ALAsset *)asset maxPixelSize:(NSUInteger)size {
+    NSParameterAssert(asset != nil);
+    NSParameterAssert(size > 0);
+    
+    ALAssetRepresentation *rep = [asset defaultRepresentation];
+    
+    CGDataProviderDirectCallbacks callbacks = {
+        .version = 0,
+        .getBytePointer = NULL,
+        .releaseBytePointer = NULL,
+        .getBytesAtPosition = getAssetBytesCallback,
+        .releaseInfo = releaseAssetCallback,
+    };
+    
+    CGDataProviderRef provider = CGDataProviderCreateDirect((void *)CFBridgingRetain(rep), [rep size], &callbacks);
+    CGImageSourceRef source = CGImageSourceCreateWithDataProvider(provider, NULL);
+    
+    CGImageRef imageRef = CGImageSourceCreateThumbnailAtIndex(source, 0, (__bridge CFDictionaryRef) @{
+                                                                  (NSString *)kCGImageSourceCreateThumbnailFromImageAlways : @YES,
+                                                                  (NSString *)kCGImageSourceThumbnailMaxPixelSize : [NSNumber numberWithInt:size],
+                                                                  (NSString *)kCGImageSourceCreateThumbnailWithTransform : @YES,
+                                                              });
+    
+    CFRelease(source);
+    CFRelease(provider);
+
+    if (!imageRef) {
+        return nil;
+    }
+    
+    UIImage *toReturn = [UIImage imageWithCGImage:imageRef];
+    
+    CFRelease(imageRef);
+    
+    return toReturn;
+}
 
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
@@ -50,12 +108,25 @@
     for (int i = 0; i < [_rowAssets count]; ++i) {
 
         ELCAsset *asset = [_rowAssets objectAtIndex:i];
+	    
+	UIImage *iuImage;
+        
+        if(asset.asset.thumbnail != NULL){
+            iuImage = [UIImage imageWithCGImage:asset.asset.thumbnail];
+        }
+        else{
+            iuImage = [self thumbnailForAsset:asset.asset maxPixelSize:200];
+        }
 
         if (i < [_imageViewArray count]) {
             UIImageView *imageView = [_imageViewArray objectAtIndex:i];
-            imageView.image = [UIImage imageWithCGImage:asset.asset.thumbnail];
+            imageView.image = iuImage;
+	    [imageView setContentMode:UIViewContentModeScaleAspectFill];
+            [imageView setClipsToBounds:TRUE];
         } else {
-            UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageWithCGImage:asset.asset.thumbnail]];
+            UIImageView *imageView = [[UIImageView alloc] initWithImage:iuImage];
+	    [imageView setContentMode:UIViewContentModeScaleAspectFill];
+            [imageView setClipsToBounds:TRUE];
             [_imageViewArray addObject:imageView];
         }
         
